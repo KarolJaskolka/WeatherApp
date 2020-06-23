ce37b727e061afc06690670059b946b146f9902c
KarolJaskolka <jaskolka.karol@gmail.com>
2020-06-23 15:11:04 +0200

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LocationComponent } from './components/location/location.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'location',
+    component: LocationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
